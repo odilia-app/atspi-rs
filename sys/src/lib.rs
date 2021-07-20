8a1960094df3d51822f014de53a99a57f578cc1a
mcb2003 <mikeybuchan@hotmail.co.uk>
2021-07-20 15:10:45 +0100

    Patch src/lib.rs to include libc::timeval.
    Because this is also used in a struct that implements Debug, we now depend
    on libc with feature extra_traits so that timeval has a debug implementation.
    
    I also added a lib.rs.patch file to easily apply this change when the bindings
    are regenerated.

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -13,6 +13,7 @@ use gobject_sys as gobject;
 use libc::{c_int, c_char, c_uchar, c_float, c_uint, c_double,
     c_short, c_ushort, c_long, c_ulong,
     c_void, size_t, ssize_t, intptr_t, uintptr_t, time_t, FILE};
+use libc::timeval;
 
 #[allow(unused_imports)]
 use glib::{gboolean, gconstpointer, gpointer, GType};
