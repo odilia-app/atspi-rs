498fbc55e96c8792ed2cc6b3fd26114ab835ffc8
mcb2003 <mikeybuchan@hotmail.co.uk>
2021-07-20 22:45:56 +0100

    Improve list-applications example by showing child count.
    The output is also now in table form.

--- examples/list-applications.rs
+++ examples/list-applications.rs
@@ -5,12 +5,16 @@ fn main() {
         eprintln!("Error initialising at-spi");
         std::process::exit(1);
     }
+
     let desktop = atspi::desktop(0).expect("Desktop 0 should exist");
+
+    println!("Index Child Count Name");
     for i in 0..desktop.child_count().expect("Could not get child count") {
         let child = desktop
             .child_at_index(i)
             .expect("Could not get child of desktop");
         let name = child.name().expect("Could not get name");
-        println!("{:4} {}", i, name);
+        let child_count = child.child_count().expect("Could not get child count");
+        println!("{:4}  {:4}         {}", i, child_count, name);
     }
 }
