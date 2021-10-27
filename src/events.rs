a8171cc2a8589833f7d6f69a5b81f25fbde129e7
Michael Connor Buchan <mikey@blindcomputing.org>
2021-10-27 17:42:15 +0100

    Events: Implement getters for fields of DeviceEvent

--- src/events.rs
+++ src/events.rs
@@ -1,10 +1,15 @@
-use std::os::raw::c_int;
+use std::{
+    os::raw::{c_int, c_uint, c_ushort},
+    time::SystemTime,
+};
 
 use glib::translate::*;
 
 use crate::accessible::Accessible;
 
-pub use crate::auto::{traits::EventListenerExt, DeviceListener, Event, EventListener, EventType};
+pub use crate::auto::{
+    traits::EventListenerExt, DeviceEvent, DeviceListener, Event, EventListener, EventType,
+};
 
 pub trait EventExt {
     fn detail1(&self) -> c_int;
@@ -55,6 +60,67 @@ impl EventExt for Event {
         unsafe {
             let kind = (*stash.0).type_;
             from_glib_none(kind)
+        }
+    }
+}
+
+pub trait DeviceEventExt {
+    fn kind(&self) -> EventType;
+    fn id(&self) -> c_uint;
+    fn hw_code(&self) -> c_ushort;
+    fn modifiers(&self) -> c_ushort;
+    fn timestamp(&self) -> SystemTime;
+    fn event_string(&self) -> glib::GString;
+    fn is_text(&self) -> bool;
+}
+
+impl DeviceEventExt for DeviceEvent {
+    fn kind(&self) -> EventType {
+        let stash = self.to_glib_none();
+        unsafe {
+            let kind = (*stash.0).type_;
+            EventType::from_glib(kind)
+        }
+    }
+
+    fn id(&self) -> c_uint {
+        let stash = self.to_glib_none();
+        unsafe { (&*stash.0).id }
+    }
+
+    fn hw_code(&self) -> c_ushort {
+        let stash = self.to_glib_none();
+        unsafe { (&*stash.0).hw_code }
+    }
+
+    fn modifiers(&self) -> c_ushort {
+        let stash = self.to_glib_none();
+        unsafe { (&*stash.0).modifiers }
+    }
+
+    fn timestamp(&self) -> SystemTime {
+        // Get the raw guint value
+        let stash = self.to_glib_none();
+        let ts = unsafe { (&*stash.0).timestamp };
+        // Convert it into a Duration
+        let dur = std::time::Duration::from_secs(ts as _);
+        // Convert this to a SystemTime using the UNIX_EPOCH
+        std::time::UNIX_EPOCH + dur
+    }
+
+    fn event_string(&self) -> glib::GString {
+        let stash = self.to_glib_none();
+        unsafe {
+            let event_string = (*stash.0).event_string;
+            from_glib_none(event_string)
+        }
+    }
+
+    fn is_text(&self) -> bool {
+        let stash = self.to_glib_none();
+        unsafe {
+            let is_text = (*stash.0).is_text;
+            bool::from_glib(is_text)
         }
     }
 }
